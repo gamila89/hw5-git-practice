e1525af16fcf2af1cb664b62594ce8cf498edc83
Gamila Elshobary <gamilaelshobary@Gamilas-MacBook-Pro-2.local>
2021-03-14 17:06:34 -0400

    modified code in file

--- hw5_git.py
+++ hw5_git.py
@@ -1,1 +1,19 @@
-print("Hello World")
+print("Hey there!")
+
+joke = input("Tell me something funny.")
+dad_jokes = []
+if "what" in joke:
+    print("what?")
+    j1 = input()
+    print("Ok, get outta here.")
+    dad_jokes.append(joke)
+    dad_jokes.append(j1)
+else:
+    if "why" in joke:
+        print("Why?")
+        j2 = input()
+        print("bahaha!")
+        dad_jokes.append(joke)
+        dad_jokes.append(j2)
+
+print(dad_jokes)
